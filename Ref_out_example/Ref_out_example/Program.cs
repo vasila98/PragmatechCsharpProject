0785bce824247625a1683adb83f78ebd9a05f99d
vasila98 <vesiih01@gmail.com>
2021-12-29 17:31:35 +0300

    ref and out example

---
+++ Ref_out_example/Ref_out_example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace Ref_ve_out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int a = 5;
+           
+           
+            ChangeNumber( ref a);
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+
+
+        }
+
+
+
+
+
+
+        static void ChangeNumber(ref int num)
+        {
+
+            num++;
+            
+
+        }
+    }
+}
