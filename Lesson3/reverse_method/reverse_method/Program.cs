5e0bae933b897c19646ec433bb6da0f38d67a954
vasila98 <vesiih01@gmail.com>
2022-01-02 00:34:21 +0400

    hazir metoddan istifade etmedim, ozum metod yazdim

---
+++ Lesson3/reverse_method/reverse_method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir eded daxil edin:");
+            int eded=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tersine yazilis:");
+            Reverse(eded);
+
+
+        }
+
+        public static int Reverse(int eded)
+        {
+            for (int i = eded; eded> -1; eded--)
+            {
+                Console.WriteLine(i--);
+
+            }
+            return eded;
+        }
+        
+   
+    }
+}
