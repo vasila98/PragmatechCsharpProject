297da296882ac64caa16e4c8d56e6e7e3d82ddb1
vasila98 <vesiih01@gmail.com>
2022-01-01 17:29:20 +0400

    String methods

---
+++ String_methods/String_methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumle = "  Mene hami Vesi deyir  ";
+            cumle = cumle.ToUpper();
+            Console.WriteLine(cumle);   
+            cumle = cumle.ToLower();
+            Console.WriteLine(cumle);
+            cumle = cumle.Trim();
+            Console.WriteLine(cumle);
+            bool isContains = cumle.Contains("vesi");
+            Console.WriteLine(isContains);
+            string username = "  ";
+            Console.WriteLine(String.IsNullOrWhiteSpace(username));
+        }
+    }
+}
