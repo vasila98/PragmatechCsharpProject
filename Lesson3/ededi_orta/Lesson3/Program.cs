864b947668550d2e5103edf8a6f874ea417aa339
vasila98 <vesiih01@gmail.com>
2022-01-01 18:07:03 +0400

    String mrthods, arrays

---
+++ Lesson3/ededi_orta/Lesson3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Birinci ededi daxil edin: ");
+            int birinci=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci ededi daxil edin: ");
+            int ikinci=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ucuncu ededi daxil edin: ");
+            int ucuncu=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dorduncu ededi daxil edin: ");
+            int dorduncu=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Besinci ededi daxil edin: ");
+            int besinci=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ededi orta:  ");
+
+            Console.WriteLine(EdediOrta(birinci,ikinci,ucuncu,dorduncu,besinci));
+        }
+        public static int EdediOrta(int birinci, int ikinci, int ucuncu, int dorduncu, int besinci)
+        {
+            int hesab= (birinci+ikinci+ucuncu+dorduncu+besinci)/5;
+            return hesab;
+        }
+    }
+}
