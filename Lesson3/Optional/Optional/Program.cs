4937887c2c7d93ee1843225ddb74c32f946968df
vasila98 <vesiih01@gmail.com>
2022-01-04 13:38:06 +0400

    replace metodundan istifade etdim

---
+++ Lesson3/Optional/Optional/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Optional("Men C# oyrenmeye calisiram");
+        }
+
+        public static void Optional(string soz)
+        {
+             Console.WriteLine(soz.Replace(" ",""));
+        }
+    }
+}
