36d8670b652fb34fa64f1e9c05741a8079ae0bab
vasila98 <vesiih01@gmail.com>
2022-01-05 18:43:17 +0400

    verilen edede qeder kvadratlari cemini hesablayan alqoritm yazdim

---
+++ Lesson3/Algoritm/Algoritm/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zehmet olmasa bir reqem daxil edin:");
+            
+            int x=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("verilen edede qeder kvadratlari cemi: ");
+            Hasil(x);
+
+            
+        }
+
+        public static int Hasil(int kv)
+        {
+            int a = 0;
+            int cem = 0;
+            for (int i = 1; i <= kv; i++)
+            {
+                a = (i * i);
+                cem += a;
+            }
+            Console.WriteLine(cem);
+            return cem;
+        }
+
+       
+
+    }
+}
