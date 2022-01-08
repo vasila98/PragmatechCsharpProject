cdcad26059a80b313f5ee5944ca9152b463c080a
vasila98 <vesiih01@gmail.com>
2022-01-08 12:36:01 +0400

    task01

--- Lesson4/Task01/Task01/Program.cs
+++ Lesson4/Task01/Task01/Program.cs
@@ -10,7 +10,9 @@ namespace Task01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Task");
+            Console.WriteLine("Task 1: ");
+            // bool a=(3+4*2==2*3)&&(true+false/2)
+            // ifadenin neticesi False olacaq. Cunki birinci terefin neticesi false-dir.
         }
     }
 }
