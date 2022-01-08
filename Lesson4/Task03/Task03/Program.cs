024fac3135d4efbdc2066ad4a2e067e5e7fec740
vasila98 <vesiih01@gmail.com>
2022-01-08 14:01:01 +0400

    ededin cut ve ya tek oldugunu gosteren metod yazdim

---
+++ Lesson4/Task03/Task03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Tek( 14587));
+        }
+        public static int Tek(int a)
+        {
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("cut ededdir");
+            }
+            else
+            {
+                Console.WriteLine("tek ededdir");
+            }
+            
+            return a;
+        }
+
+    }
+}
