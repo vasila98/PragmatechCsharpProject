849555f36229cfa07c8ae4fc16ba0ce11118aec5
vasila98 <vesiih01@gmail.com>
2022-01-08 14:17:25 +0400

    if else emeliyyatlari...

---
+++ Lesson4/Task04/Task04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yasinizi daxil edin : ");
+            int yas=Convert.ToInt32(Console.ReadLine());
+            if (yas < 30 && yas>0)
+            {
+                Console.WriteLine("Yasinizin kvadrati : ");
+                Console.WriteLine(yas * yas);
+            }
+            else if (30 < yas && yas < 40 && yas>0)
+            {
+                Console.WriteLine("son reqem");
+            }
+            else 
+            {
+                Console.WriteLine("Duzgun melumat daxil etmemisiniz");
+
+            }
+        }
+    }
+}
