ba4c046633c32b839b17c4119847e42c2b2305a1
vasila98 <vesiih01@gmail.com>
2022-01-15 20:35:04 +0400

    class obyekt

---
+++ Lesson7/Task/Task/Task/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+
+
+        public Student(string usrname, string surnm)
+        {
+            Name= usrname;
+            Surname= surnm;
+        }
+    }
+}
---
+++ Lesson7/Task/Task/Task/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Vesi", "Hesenova");
+            Console.WriteLine(student.Name);
+        }
+    }
+}
