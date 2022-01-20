77220a62c195e24fcada38299a7160b3c9c1f4b7
vasila98 <vesiih01@gmail.com>
2022-01-20 23:11:52 +0400

    book classi

---
+++ Lesson8/BookClass/BookClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("book classi");
+        }
+    }
+}
