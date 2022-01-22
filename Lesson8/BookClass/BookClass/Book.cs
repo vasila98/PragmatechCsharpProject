aa316b7c4f9cedfa439d6dd3fa5fc9099477e92a
vasila98 <vesiih01@gmail.com>
2022-01-22 11:32:04 +0400

    book class

---
+++ Lesson8/BookClass/BookClass/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClass
+{
+    internal class Book
+    {
+        public string Name;
+        public int PageCount;
+        
+
+        public Book(string nm, int pgcnt)
+        {
+            Name = nm;
+            PageCount = pgcnt;
+        }
+    }
+}
