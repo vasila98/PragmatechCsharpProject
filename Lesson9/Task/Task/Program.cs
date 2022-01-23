df42c3b12e99549f68bd5ddde14dd4259902c38a
vasila98 <vesiih01@gmail.com>
2022-01-23 21:01:02 +0400

    dictonary list

---
+++ Lesson9/Task/Task/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> olkevepytxt=new Dictionary<string, string>();
+            for (int i = 0; i <2; i++)
+            {
+                Console.WriteLine("olke");
+                string olke=Console.ReadLine();
+                Console.WriteLine("seher");
+                string paytaxt=Console.ReadLine();
+                olkevepytxt.Add(olke, paytaxt);
+                
+              
+             
+
+            }
+            Console.WriteLine("yaz");
+            string olkead=Console.ReadLine();
+            foreach (var item in olkevepytxt)
+            {
+                if (item.Key == olkead)
+                {
+                    Console.WriteLine(item.Value);
+                }
+            }
+            Console.WriteLine("daxil ele");
+            string yes = "y";
+            string no = "n";
+            string son=Console.ReadLine();
+            if (son == yes)
+            {
+                
+
+            }
+            else if(son == no)
+            {
+                Console.WriteLine("sagol");
+                
+            }
+
+        }
+    }
+}
