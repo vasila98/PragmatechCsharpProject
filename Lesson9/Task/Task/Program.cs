6e21f3d1e553340766a3188a055ef40f0c2f4693
vasila98 <vesiih01@gmail.com>
2022-01-26 12:07:21 +0400

    list

--- Lesson9/Task/Task/Program.cs
+++ Lesson9/Task/Task/Program.cs
@@ -108,6 +108,39 @@ namespace Task
     
 
                
+            // task 3
+            //List<int> reqemler = new List<int>();
+            //List<int> reqemler2 = new List<int>();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("reqem daxil ele: ");
+            //    int reqem=Convert.ToInt32(Console.ReadLine());
+            //    reqemler.Add(reqem);
+            //}
+            //for (int k = 6;k < 11; k++)
+            //{
+            //    reqemler2.Add(k);
+            //}
+            //for (int h = 1;h < 6; h++)
+            //{
+            //    reqemler2.Add(h);
+            //}
+           
+
+
+            //foreach (int i in reqemler2)
+            //{
+                
+            //    Console.WriteLine("reqem..."+i);
+            //}
+
+
+
+
+
+
+
+
 
                 // task 4
                 //SortedSet<int> set = new SortedSet<int>();
