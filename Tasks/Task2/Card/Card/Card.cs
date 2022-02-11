3a72b207fb2df45cd9eedc4176e8ea4b1a867519
vasila98 <vesiih01@gmail.com>
2022-02-11 17:39:51 +0400

    .

---
+++ Tasks/Task2/Card/Card/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card
+{
+    internal class Card
+    {
+        public long Kod;
+        public int Mebleg;
+        public string Vaxt;
+
+        public Card(long kd, int mebleg,string vxt)
+        {
+            Kod = kd;
+            Mebleg = mebleg;    
+            Vaxt = vxt;
+           
+        }
+
+        public static void CreateCard()
+        {
+            Console.WriteLine( "ne qeder mebleg ayirmisiniz: ");
+            int mebleg=Convert.ToInt32(Console.ReadLine());
+            if ( )
+            {
+
+            }
+            Console.WriteLine("16 xanali kodu daxil edin: ");
+            double kod=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ne qeder muddetlik kart isteyirsiz");
+            string vaxt=Console.ReadLine();
+        }
+
+       
+
+
+    }
+}
---
+++ Tasks/Task2/Card/Card/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Card obyekt = new Card(56654455, 55, "5gun");
+        }
+    }
+}
