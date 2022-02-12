41c3f642226367f8242441f0b32480fd6c6fdd27
vasila98 <vesiih01@gmail.com>
2022-02-12 21:37:35 +0400

    emeliyyatlar

--- Tasks/Task2/Card/Card/Program.cs
+++ Tasks/Task2/Card/Card/Program.cs
@@ -12,7 +12,52 @@ namespace Card
         {
             Card.KartaPulElaveEt();
             Card.KartinKodunuELaveEt();
-            Card.KartdakiPuluGoster();
+          
+         
+            while (true)
+            {
+               
+                Console.WriteLine("1.virtual kart yaratmaq.");
+                Console.WriteLine("2.virtual kartlari gosterir.");
+                Console.WriteLine("3.esas kartdaki pulu gosterir.");
+                Console.WriteLine("4.Cixis");
+                Console.WriteLine("bir reqem secin");
+                string secim=Console.ReadLine();
+              
+                if ( secim == "1")
+                {
+                    if (Card.Mebleg != 0)
+                    {
+                        Console.WriteLine("kartda ne qeder pul var");
+                        int pul=Convert.ToInt32(Console.ReadLine());
+                        VirtualCard card=new VirtualCard(pul);
+                        VirtualCard.Kartlar(card);
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("kartinizda pul qalmayib.");
+                    }
+                    
+                   
+                   
+                }
+                if (secim == "2")
+                {
+                    VirtualCard.VirtualKartlariGoster();
+                }
+                if (secim == "3")
+                {
+                    Card.KartdakiPuluGoster();
+                }
+                if (secim == "4")
+                {
+                    Console.WriteLine("emeliyyat sona catmisdir.");
+                    break;
+                }
+            }
+
+           
             
         }
     }
