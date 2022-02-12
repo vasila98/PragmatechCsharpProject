a2fab58b13894cce958e85050f939c964cbc8d1d
vasila98 <vesiih01@gmail.com>
2022-02-12 15:46:30 +0400

    esas kart

--- Tasks/Task2/Card/Card/Card.cs
+++ Tasks/Task2/Card/Card/Card.cs
@@ -15,12 +15,14 @@ namespace Card
         {
             Console.WriteLine("kartinizin 16 xanali kodu :");
             Random random = new Random();
+            string a;
             for (int i = 0; i < 16; i++)
             {
 
 
-                long kod = random.Next(9);
+                 long kod = random.Next(9);
                 Console.WriteLine(kod.ToString());
+               
             }
           
 
