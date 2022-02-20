2d5886449c6e504fddbdca5e99ed370f711a85c3
vasila98 <vesiih01@gmail.com>
2022-02-21 01:21:23 +0400

    var ve dinamikin ferqi

---
+++ Algorithms/var_dynamic/var_dynamic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace var_dynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 5;
+            a = 666;
+            a = 'g';
+            dynamic b = 5;
+            b = 6;
+            b = "vesi";
+            b = true;
+            
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+              
+        }
+    }
+}
