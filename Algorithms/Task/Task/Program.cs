ee521534cb5e2b8fa4602a4858361d0cc4dee7f3
vasila98 <vesiih01@gmail.com>
2022-02-20 20:09:29 +0400

    .

---
+++ Algorithms/Task/Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] adlar = { "Rehman", "Aysel", "Vesi", "Elmar" };
+            string ad =" ";
+            foreach (string a in adlar)
+            {
+                ad += a[0];
+            }
+           
+           
+            Console.WriteLine(ad);
+            
+        }
+    }
+}
---
+++ Algorithms/sahePerimetr/sahePerimetr/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sahePerimetr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kvadratin terefini daxil edin: ");
+            Kvadrat kv = new Kvadrat();
+ 
+            kv.Perimetr();
+            kv.Sahe();
+            Console.WriteLine("Duzbucaqlinin tereflerini daxil edin: ");
+            Duzbucaqli db = new Duzbucaqli();
+            db.Perimetr();
+            db.Sahe();
+
+        }
+        interface IFigure
+        {
+            void Sahe();
+            void Perimetr();
+        }
+        class Kvadrat : IFigure
+        {
+           
+            int a=Convert.ToInt32(Console.ReadLine());
+            public void Perimetr()
+            {
+                int pm = 4 * a;
+                Console.WriteLine("kvadratin perimetri: "+pm);
+            }
+
+            public void Sahe()
+            {
+                int sahe = a * a;
+                Console.WriteLine("kvadratin sahesi: "+sahe);
+            }
+        }
+        class Duzbucaqli : IFigure
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            public void Perimetr()
+            {
+                int pm=2*(a+b);
+                Console.WriteLine("Duzbucaqlinin perimetri: "+pm);
+            }
+
+            public void Sahe()
+            {
+                int sahe = a * b;
+                Console.WriteLine("Duzbucaqlinin sahesi : " + sahe);
+            }
+        }
+        class Ucbucaq : IFigure
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+           
+
+            public void Perimetr()
+            {
+                int pm = a + b + c;
+                Console.WriteLine("ucbucaqlinin perimetri: "+pm);
+            }
+
+            public void Sahe()
+            {
+                int sahe = (a * b) / 2;
+                Console.WriteLine("ucbucaqlinin sahesi: "+sahe);
+            }
+        }
+    }
+}
