7ff8d146232c819ef129476fd3347655690bc51a
vasila98 <vesiih01@gmail.com>
2022-02-22 20:56:10 +0400

    namespace

---
+++ Lessons/Lesson14/Namespace/Namespace/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NN.Vesi.Adgoster("vesi",22);
+            BB.Vesi.Adgoster("hesen", 55);
+
+
+        }
+    }
+}
+namespace NN
+{
+    public class Vesi
+    {
+        public string Name;
+        public int Age;
+
+        public static void Adgoster(string nm, int yas)
+        {
+           
+            Console.WriteLine(nm+" "+yas);
+        }
+    }
+
+}
+namespace BB
+{
+    public class Vesi
+    {
+
+        public string Name;
+        public int Age;
+
+        public static void Adgoster(string nm, int yas)
+        {
+            
+            Console.WriteLine(nm + " " + yas);
+        }
+      
+    }
+
+}
