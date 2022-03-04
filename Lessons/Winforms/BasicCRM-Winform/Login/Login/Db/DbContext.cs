a2023688766002d36aefd429093bce9d526a9057
vasila98 <vesiih01@gmail.com>
2022-03-04 23:18:29 +0400

    task

---
+++ Lessons/Winforms/BasicCRM-Winform/Login/Login/Db/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login.Db
+{
+    internal class DbContext
+    {
+        public static List<Models.User> Users = new List<Models.User>();
+    }
+}
